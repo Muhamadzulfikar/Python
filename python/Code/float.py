6f48d6b237fbab7ad751926212a15e0ca375967f
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-01-17 19:16:24 +0700

    Delete python/Code directory

--- python/Code/float.py
+++
@@ -1,3 +0,0 @@
-a = float(input("Masukan A :"))
-b = float(input("Masukan B :"))
-print(a+b)
--- python/Code/main.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Masukan nilai A :"))
-b=int(input("Masukan Nilai B :"))
-
-print("\n Nilai yang dibaca")
-print("A:",a)
-print("B:",b)
