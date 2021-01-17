496fe1dd02aea7060b46892a1f1f9aa1fd3df530
Muhamad Zulfikar <65901202+Muhamadzulfikar@users.noreply.github.com>
2021-01-17 19:19:07 +0700

    Delete main.py

--- python/main.py
+++
@@ -1,6 +0,0 @@
-print("Program menghitung bangun datar");
-print("Program menghitung persegi panjang");
-panjang=int(input("Masukan panjang"));
-lebar=int(input("Masukan lebar"));
-persegipanjang=panjang*lebar;
-print("Hasil persegi panjang adalah:",persegipanjang);
